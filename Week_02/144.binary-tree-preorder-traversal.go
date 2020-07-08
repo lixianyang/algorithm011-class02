13497e6cf32d80c6df2246eee97fabd4dcae02aa
Sven Lee <lixianyang@users.noreply.github.com>
2020-07-08 22:30:16 +0800

    二叉树前序遍历添加 Mirros 算法

--- Week_02/144.binary-tree-preorder-traversal.go
+++ Week_02/144.binary-tree-preorder-traversal.go
@@ -43,3 +43,28 @@ func preorderTraversal(root *TreeNode) []int {
 	}
 	return result
 }
+// Morris 遍历，O(1) 空间复杂度 参考：https://www.cnblogs.com/anniekim/archive/2013/06/15/morristraversal.html
+func preorderTraversal(root *TreeNode) []int {
+    var result []int
+    var pre *TreeNode
+    for root != nil {
+        if root.Left == nil {
+            result = append(result, root.Val)
+            root = root.Right
+        } else {
+            pre = root.Left
+            for pre.Right != nil && pre.Right != root {
+                pre = pre.Right
+            }
+            if pre.Right == nil { // 找到了前驱节点，更新右指针指向当前节点
+                pre.Right = root
+                result = append(result, root.Val) // 在往左子树走之前，输出当前节点的值
+                root = root.Left
+            } else {    // pre.Right == root 从前驱节点回退到当前节点
+                pre.Right = nil
+                root = root.Right
+            }
+        }
+    }
+    return result
+}
