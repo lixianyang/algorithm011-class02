ec40634a4db8b61ce4df242a54566af8486abb1b
Sven Lee <lixianyang@users.noreply.github.com>
2020-07-08 22:22:21 +0800

    添加 morris 中序遍历

--- Week_02/94.binary-tree-inorder-traversal.go
+++ Week_02/94.binary-tree-inorder-traversal.go
@@ -47,3 +47,29 @@ func inorderTraversal(root *TreeNode) []int {
 	}
 	return result
 }
+// Morris 遍历，利用叶子节点指针存储回退信息，空间复杂度 O(1)
+// Morris 详解：https://www.cnblogs.com/anniekim/archive/2013/06/15/morristraversal.html
+func inorderTraversal(root *TreeNode) []int {
+    var result []int
+    var predecessor *TreeNode
+    for root != nil {
+        if root.Left == nil {
+            result = append(result, root.Val)
+            root = root.Right
+        } else {
+            predecessor = root.Left
+            for predecessor.Right != nil && predecessor.Right != root {
+                predecessor = predecessor.Right
+            }
+            if predecessor.Right == nil {
+                predecessor.Right = root
+                root = root.Left    // 如果是前序，需要在往左走前，输出当前节点的值
+            } else {    // predecessor.Right == root 表示从前驱节点返回
+                result = append(result, root.Val)
+                predecessor.Right = nil
+                root = root.Right
+            }
+        }
+    }
+    return result
+}
