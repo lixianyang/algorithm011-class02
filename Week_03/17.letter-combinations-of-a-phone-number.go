df5f354d31f4739bf3317dee843e72aa6d386d37
Sven Lee <lixianyang@users.noreply.github.com>
2020-07-10 14:25:32 +0800

    更新电话号码的字母组合解法
    1. 优化内存，使用数组替换字典
    2. 添加 BFS 解法

--- Week_03/17.letter-combinations-of-a-phone-number.go
+++ Week_03/17.letter-combinations-of-a-phone-number.go
@@ -1,26 +1,62 @@
+// 回溯
 func letterCombinations(digits string) []string {
     if len(digits) == 0 { return nil }
-    m := map[byte][]byte{
-        '2': []byte{'a', 'b', 'c'},
-        '3': []byte{'d', 'e', 'f'},
-        '4': []byte{'g', 'h', 'i'},
-        '5': []byte{'j', 'k', 'l'},
-        '6': []byte{'m', 'n', 'o'},
-        '7': []byte{'p', 'q', 'r', 's'},
-        '8': []byte{'t', 'u', 'v'},
-        '9': []byte{'w', 'x', 'y', 'z'},
+    m := [][]byte {
+        []byte{'a', 'b', 'c'},
+        []byte{'d', 'e', 'f'},
+        []byte{'g', 'h', 'i'},
+        []byte{'j', 'k', 'l'},
+        []byte{'m', 'n', 'o'},
+        []byte{'p', 'q', 'r', 's'},
+        []byte{'t', 'u', 'v'},
+        []byte{'w', 'x', 'y', 'z'},
     }
     result := make([]string, 0)
     trace := make([]byte, 0, len(digits))
     backtrack(&result, []byte(digits), trace, m)
     return result
 }
-func backtrack(result *[]string, digits []byte, trace []byte, m map[byte][]byte) {
+func backtrack(result *[]string, digits []byte, trace []byte, m [][]byte) {
     if len(digits) == 0 {
         *result = append(*result, string(trace))
         return
     }
-    for _, c := range m[digits[0]] {
+    for _, c := range m[digits[0]-'2'] {
         backtrack(result, digits[1:], append(trace, c), m)
     }
 }
+// BFS 记录所有路径作为结果
+/*
+func letterCombinations(digits string) []string {
+    n := len(digits)
+    if n == 0 { return nil }
+    m := [][]byte{
+        []byte{'a', 'b', 'c'},
+        []byte{'d', 'e', 'f'},
+        []byte{'g', 'h', 'i'},
+        []byte{'j', 'k', 'l'},
+        []byte{'m', 'n', 'o'},
+        []byte{'p', 'q', 'r', 's'},
+        []byte{'t', 'u', 'v'},
+        []byte{'w', 'x', 'y', 'z'},
+    }
+    queue := [][]byte{ []byte{} }
+    for _, digit := range digits {
+        for i := len(queue); i > 0; i-- {
+            top := queue[0]
+            queue = queue[1:]
+            length := len(top)+1
+            for _, c := range m[digit-'2'] {
+                tmp := make([]byte, 0, length)
+                tmp = append(append(tmp, top...), c)
+                queue = append(queue, tmp)
+            }
+        }
+    }
+    result := make([]string, len(queue))
+    for i, arr := range queue {
+        result[i] = string(arr)
+    }
+    return result
+}
+*/
