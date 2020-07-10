41e85d7b65ffd2dfd46ddb31b5205ec706c37d2e
Sven Lee <lixianyang@users.noreply.github.com>
2020-07-10 12:43:37 +0800

    快速幂添加二进制表示法的迭代解法

--- Week_03/50.powx-n.go
+++ Week_03/50.powx-n.go
@@ -1,3 +1,4 @@
+// 分治
 func myPow(x float64, n int) float64 {
     if n < 0 {
         x = 1 / x
@@ -12,4 +13,21 @@ func pow(x float64, n int) float64 {
         return half*half*x
     }
     return half*half
+}
+// 迭代
+func myPow(x float64, n int) float64 {
+    if n == 0 { return 1 }
+    if n < 0 {
+        n = -n
+        x = 1/x
+    }
+    result := 1.0
+    for n > 0 {
+        if n & 1 == 1 {
+            result *= x
+        }
+        x *= x
+        n = n >> 1
+    }
+    return result
 }
