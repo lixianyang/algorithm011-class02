f9ec2e0a2e5e99e7062c503d6bfd48b625162f07
Sven Lee <lixianyang@users.noreply.github.com>
2020-07-14 01:20:29 +0800

    Update 529.minesweeper.go

--- Week_04/529.minesweeper.go
+++ Week_04/529.minesweeper.go
@@ -1,3 +1,5 @@
+// 递归
+/*
 func updateBoard(board [][]byte, click []int) [][]byte {
     update(board, len(board), len(board[0]), click[0], click[1])
     return board
@@ -42,4 +44,51 @@ func countM(board [][]byte, rows, cols, row, col int) byte {
         }
     }
     return n
+}
+*/
+func updateBoard(board [][]byte, click []int) [][]byte {
+	rows, cols := len(board), len(board[0])
+	queue := [][]int{click}
+	for len(queue) > 0 {
+		row, col := queue[0][0], queue[0][1]
+		queue = queue[1:]
+		if board[row][col] == 'E' {
+			board[row][col] = 'B'
+			n := countM(board, rows, cols, row, col)
+			if n == '0' {
+				for i := -1; i <= 1; i++ {
+					for j := -1; j <= 1; j++ {
+						currentRow, currentCol := row+i, col+j
+						if currentRow < 0 || currentRow >= rows || currentCol < 0 || currentCol >= cols || i == 0 && j == 0 {
+							continue
+						}
+						if board[currentRow][currentCol] == 'E' {
+							queue = append(queue, []int{currentRow, currentCol})
+						}
+					}
+				}
+			} else {
+				board[row][col] = n
+			}
+		} else if board[row][col] == 'M' {
+			board[row][col] = 'X'
+			return board
+		}
+	}
+	return board
+}
+func countM(board [][]byte, rows, cols, row, col int) byte {
+	var n byte = '0'
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			currentRow, currentCol := row+i, col+j
+			if currentRow < 0 || currentRow >= rows || currentCol < 0 || currentCol >= cols {
+				continue
+			}
+			if board[currentRow][currentCol] == 'M' {
+				n++
+			}
+		}
+	}
+	return n
 }
